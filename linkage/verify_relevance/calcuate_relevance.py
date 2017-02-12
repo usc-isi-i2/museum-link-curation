b9259b5143a5f1c34047844f859965a79b172133
Chinmay Naik <chinmayn@usc.edu>
2017-02-12 01:28:30 -0800

    Modify relevance script to calculate false/true positives/negatives

--- linkage/verify_relevance/calcuate_relevance.py
+++ linkage/verify_relevance/calcuate_relevance.py
@@ -8,7 +8,7 @@ Input
 
 
 Output
-{"precision": 0.89, "recall": 0.41}	
+{"precision": 0.89, "recall": 0.81}	
 
 
 Cases
@@ -27,32 +27,43 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import json, sys
 import museum_graph_api_config as config
 SPARQL_ENDPOINT = "http://data.americanartcollaborative.org/sparql"
+from collections import defaultdict
 
 def calculate_relevance(ipfile, museum):
 	parent_uri_set = set()
 	success_uri = set()
-	ignored_uri = set()
+	tn = set()
 	failure_uri = {}
 	total_uri_set = set()
 	sparql = SPARQLWrapper(SPARQL_ENDPOINT)
 	sparql.setReturnFormat(JSON)
-	museum_dict = {'GM': config.GM, 'IMA': config.IMA}
+	museum_dict = {'GM': 'GM', 'IMA': 'ima'}
+
+	data_dict = defaultdict(list)
 	query = ''
 
+	#Calculate the ground truth
 	#Cacluate total URI matches for the MUSUEM
 	count_query = ''
-	with open('count_uri_matches.sparql', 'r') as query_file:
+	with open('get_uri_matches.sparql', 'r') as query_file:
 		# get_ulan_uri.sparql has sparql query , need to replace PARENT_URI with actual URI
-		count_query = query_file.read()
+		get_query = query_file.read()
 	museum_uri = museum_dict[museum]
-	count_query = count_query.replace('MUSUEM_URI', museum_uri)
-	sparql.setQuery(count_query)
-	output = sparql.query().convert()
-	total_match_count = float(output['results']['bindings'][0]['.1']['value'])
+	get_query = get_query.replace('MUSUEM_URI', museum_uri)
+	sparql.setQuery(get_query)
+	result = sparql.query().convert()
 
+	ground_truth = {}
+	#convert to dict {'Actor_URI': Ulan_URI}
+	for v in result['results']['bindings']:
+		ground_truth[v['uri']['value']] = v['lod_identifier']['value']
 
+	#there are some duplicates so consider as a set
+	total_match_count = float(len(set(ground_truth.keys())))
+
+	# get_ulan_uri.sparql has sparql query to fetch ULAN ID for Actor URI,
 	with open('get_ulan_uri.sparql', 'r') as query_file:
-		# get_ulan_uri.sparql has sparql query , need to replace PARENT_URI with actual URI
+		#  need to replace PARENT_URI with actual URI
 		query = query_file.read()
 
 	with open(ipfile, 'r') as ip:
@@ -61,10 +72,20 @@ def calculate_relevance(ipfile, museum):
 			parent_uri = data["uri1"]
 			ulan_uri = data["uri2"]
 			similarity_score = float(data["similarity"]["score"])
-			#print(similarity_score)
-			#if parent_uri in parent_uri_set:
-			#	continue
-			#parent_uri_set.add(parent_uri)
+			data_dict[parent_uri].append({'ulan_uri': ulan_uri, 'similarity': similarity_score})
+
+			if parent_uri in ground_truth:
+				for v in data_dict[parent_uri]:
+					v['correct_ulan_uri'] = ground_truth[parent_uri]
+				total_uri_set.add(parent_uri)
+				if ground_truth[parent_uri] == ulan_uri:
+					success_uri.add(parent_uri)
+			else:
+				tn.add(parent_uri)
+
+
+			'''
+			data_dict[parent_uri].append({'ulan_uri': ulan_uri, 'similarity': similarity_score})
 			squery = query.replace('PARENT_URI', parent_uri)
 			sparql.setQuery(squery)
 			output = sparql.query().convert()
@@ -74,23 +95,33 @@ def calculate_relevance(ipfile, museum):
 				bindings = set()
 				for binding in output['results']['bindings']:
 					bindings.add(binding['lod_identifier']['value'])
+					for v in data_dict[parent_uri]:
+						v['correct_ulan_uri'] = binding['lod_identifier']['value']
 
 				if ulan_uri in bindings:
 					success_uri.add(parent_uri)
 			else:
-				ignored_uri.add(parent_uri)
+				tn.add(parent_uri)
+			'''
 	precision = 0
 	recall = 0
 	if len(total_uri_set) > 0:
 		precision = float(len(success_uri)) / (len(total_uri_set))
 		recall = float(len(success_uri)) / total_match_count
-	#print(len(success_uri), len(failure_uri), len(total_uri_set))
-	#failure_uri = total_uri_set - success_uri
+
 	failures = list(total_uri_set - success_uri)
+	false_positives = defaultdict(list)
+	for f in failures:
+		false_positives[f] = data_dict[f]
+	true_negatives = defaultdict(list)
+	for f in tn:
+		true_negatives[f] = data_dict[f]
 
-
-	result = {'precision': precision, 'recall': recall,'false_positive_results': failures}
+	false_negatives = list(set(ground_truth.keys()) - set(data_dict.keys()))
+	result = {'false_positives': false_positives , 'false_negatives': false_negatives, 'true_negatives': true_negatives,
+				'total': [len(total_uri_set),len(data_dict.keys()), total_match_count], 'success': len(success_uri), 'precision': precision, 'recall': recall}
 	print(json.dumps(result))
+
 
 if __name__ == '__main__':
 	#print(len(sys.argv))
